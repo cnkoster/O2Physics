a4b13d5aec8e355ff713667922ff1e05dd003f52
cnkoster <noor.koster@cern.ch>
2024-06-24 15:23:31 +0200

    Added working task to calculate mean energy for ZDC towers and added to CMakeLists file

--- PWGCF/Flow/Tasks/ZDCMeanEnergy.cxx
+++ PWGCF/Flow/Tasks/ZDCMeanEnergy.cxx
@@ -40,9 +40,21 @@ using namespace o2::framework;
 using namespace o2::framework::expressions;
 //using namespace o2::analysis;
 
+namespace o2::analysis::test{
+std::shared_ptr<TProfile2D> histograms[10] = {{nullptr}};
+}
+using namespace o2::analysis::test;
 #define O2_DEFINE_CONFIGURABLE(NAME, TYPE, DEFAULT, HELP) Configurable<TYPE> NAME{#NAME, DEFAULT, HELP};
 
 struct ZDCAnalysis {
+  
+  ConfigurableAxis axisCent{"axisCent", {90,0,90},"Centrality axis in 1% bins"};
+  
+  O2_DEFINE_CONFIGURABLE(cfgCutVertex,        float,      10.0f, "Accepted z-vertex range")
+  O2_DEFINE_CONFIGURABLE(cfgCutPtMin,         float,      0.2f, "Minimal pT for ref tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutPtMax,         float,      3.0f, "Maximal pT for ref tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutEta,           float,      0.8f, "Eta range for tracks")
+  O2_DEFINE_CONFIGURABLE(cfgCutChi2prTPCcls,  float,      2.5, "Chi2 per TPC clusters")
   
   //Filters
   Filter collisionFilter = nabs(aod::collision::posZ) < cfgCutVertex;
@@ -54,7 +66,6 @@ struct ZDCAnalysis {
   
   //define my.....
   using myTracks = soa::Filtered<soa::Join<aod::Tracks, aod::TracksExtra, aod::TrackSelection, aod::TracksDCA>>;
-  
   using BCsRun3 = soa::Join<aod::BCs, aod::Timestamps, aod::BcSels, aod::Run3MatchedToBCSparse>;
   
   //define global parameters
@@ -64,85 +75,77 @@ struct ZDCAnalysis {
   
   void init(InitContext const&)
   {
-    
-
-    //Tower mean energies vs. centrality
-    registry.add("hZNA_mean_t1_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNA_mean_t2_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNA_mean_t3_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNA_mean_t4_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNA_mean_common_cent","", {HistType::kTProfile, {{axisCent}}});
-    
-    registry.add("hZNC_mean_t1_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNC_mean_t2_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNC_mean_t3_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNC_mean_t4_cent","", {HistType::kTProfile, {{axisCent}}});
-    registry.add("hZNC_mean_common_cent","", {HistType::kTProfile, {{axisCent}}});
+    //Tower mean energies vs. centrality ( common=t0 )
+    for(int tower = 0; tower<5; tower++){
+      histograms[tower] = registry.add<TProfile2D>(Form("hZNA_mean_t%i_cent", tower),"", kTProfile2D, {{1,0,1},axisCent});
+      histograms[tower+5] = registry.add<TProfile2D>(Form("hZNC_mean_t%i_cent", tower),"", kTProfile2D, {{1,0,1},axisCent});
+    }
     
   }
   
-  void processRun2(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::CentRun2V0Ms>>::iterator const& collision, aod::Zdcs const&, myTracks const& tracks) //
-
+  void processRun2(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::CentRun2V0Ms>>::iterator const& collision, 
+                   aod::Zdcs const&,
+                   myTracks const& tracks)
   {
-      int Ntot = tracks.size();
-      if (Ntot < 1)
-        return;
-      
-      if (!collision.sel7())
-        return;
-      auto cent = collision.centRun2V0M();
-      if(cent<0 || cent>100)
-        return;
-      
-      if (collision.foundZDCId() >= 0) { //
-        
-        registry.fill(HIST("hZNA_mean_common_cent"),int(cent),collision.foundZDC().energyCommonZNA());
-        registry.fill(HIST("hZNA_mean_t1_cent"),int(cent),collision.foundZDC().energySectorZNA()[0]);
-        registry.fill(HIST("hZNA_mean_t2_cent"),int(cent),collision.foundZDC().energySectorZNA()[1]);
-        registry.fill(HIST("hZNA_mean_t3_cent"),int(cent),collision.foundZDC().energySectorZNA()[2]);
-        registry.fill(HIST("hZNA_mean_t4_cent"),int(cent),collision.foundZDC().energySectorZNA()[3]);
-        
-        registry.fill(HIST("hZNC_mean_common_cent"),int(cent),collision.foundZDC().energyCommonZNC());
-        registry.fill(HIST("hZNC_mean_t1_cent"),int(cent),collision.foundZDC().energySectorZNC()[0]);
-        registry.fill(HIST("hZNC_mean_t2_cent"),int(cent),collision.foundZDC().energySectorZNC()[1]);
-        registry.fill(HIST("hZNC_mean_t3_cent"),int(cent),collision.foundZDC().energySectorZNC()[2]);
-        registry.fill(HIST("hZNC_mean_t4_cent"),int(cent),collision.foundZDC().energySectorZNC()[3]);
- 
+    int Ntot = tracks.size();
+    if (Ntot < 1)
+      return;
+    
+    if (!collision.sel7())
+      return;
+    auto cent = collision.centRun2V0M();
+    if(cent<0 || cent>90)
+      return;
+    
+    int runnumber = collision.bc().runNumber();
+    
+    if (collision.foundZDCId() >= 0) { //
+    
+      for(int tower = 0; tower<5; tower++){
+        if(tower==0){
+          histograms[tower]->Fill(Form("%d", runnumber), int(cent),collision.foundZDC().energyCommonZNA(),1);
+          histograms[tower+5]->Fill(Form("%d", runnumber), int(cent),collision.foundZDC().energyCommonZNA(),1);
+        } else {
+          histograms[tower]->Fill(Form("%d", runnumber), int(cent),collision.foundZDC().energySectorZNA()[tower-1],1);
+          histograms[tower+5]->Fill(Form("%d", runnumber), int(cent),collision.foundZDC().energySectorZNC()[tower-1],1);
+        }
       }
+    }
   }
   PROCESS_SWITCH(ZDCAnalysis, processRun2, "Processing ZDC mean energy for run 2", false);
   
-  void processRun3(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::CentFT0Cs>>::iterator const& collision, aod::BCsWithTimestamps const&, BCsRun3 const& bcs, aod::Zdcs const&, myTracks const& tracks) //
-
+  void processRun3(soa::Filtered<soa::Join<aod::Collisions, aod::EvSels, aod::CentFT0Cs>>::iterator const& collision,
+                   BCsRun3 const& bcs,
+                   aod::Zdcs const&,
+                   myTracks const& tracks)
   {
-      int Ntot = tracks.size();
-      if (Ntot < 1)
-        return;
-      
-      if (!collision.sel8())
-        return;
-      auto cent = collision.centFT0Cs();
-      if(cent<0 || cent>90)
-        return;
-      
+    int Ntot = tracks.size();
+    if (Ntot < 1)
+      return;
+    
+    if (!collision.sel8())
+      return;
+    auto cent = collision.centFT0C();
+    if(cent<0 || cent>90)
+      return;
+    
     const auto& foundBC = collision.foundBC_as<BCsRun3>();
     if(foundBC.has_zdc()){
-
-      zdcCol = foundBC.zdc();
       
-        registry.fill(HIST("hZNA_mean_common_cent"),int(cent),zdcCol.energyCommonZNA());
-        registry.fill(HIST("hZNA_mean_t1_cent"),int(cent),zdcCol.energySectorZNA()[0]);
-        registry.fill(HIST("hZNA_mean_t2_cent"),int(cent),zdcCol.energySectorZNA()[1]);
-        registry.fill(HIST("hZNA_mean_t3_cent"),int(cent),zdcCol.energySectorZNA()[2]);
-        registry.fill(HIST("hZNA_mean_t4_cent"),int(cent),zdcCol.energySectorZNA()[3]);
-        
-        registry.fill(HIST("hZNC_mean_common_cent"),int(cent),zdcCol.energyCommonZNC());
-        registry.fill(HIST("hZNC_mean_t1_cent"),int(cent),zdcCol.energySectorZNC()[0]);
-        registry.fill(HIST("hZNC_mean_t2_cent"),int(cent),zdcCol.energySectorZNC()[1]);
-        registry.fill(HIST("hZNC_mean_t3_cent"),int(cent),zdcCol.energySectorZNC()[2]);
-        registry.fill(HIST("hZNC_mean_t4_cent"),int(cent),zdcCol.energySectorZNC()[3]);
- 
-      } // end of has_zdc()
+      int runnumber = foundBC.runNumber();
+      auto zdcCol = foundBC.zdc();
+    
+      for(int tower = 0; tower<5; tower++){
+        if(tower==0){
+          if(zdcCol.energyCommonZNA()>0)histograms[tower]->Fill(Form("%d", runnumber), int(cent), zdcCol.energyCommonZNA(),1);
+          if(zdcCol.energyCommonZNA()>0)histograms[tower+5]->Fill(Form("%d", runnumber), int(cent), zdcCol.energyCommonZNA(),1);
+        } else {
+          if(zdcCol.energySectorZNA()[tower-1]>0)histograms[tower]->Fill(Form("%d", runnumber), int(cent), zdcCol.energySectorZNA()[tower-1],1);
+          if(zdcCol.energySectorZNC()[tower-1]>0)histograms[tower+5]->Fill(Form("%d", runnumber), int(cent), zdcCol.energySectorZNC()[tower-1],1);
+        }
+      }
+      
+    } // end of has_zdc()
   }
   PROCESS_SWITCH(ZDCAnalysis, processRun3, "Processing ZDC mean energy for run 3", false);
 };
@@ -151,6 +154,6 @@ WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   return WorkflowSpec{
     adaptAnalysisTask<ZDCAnalysis>(cfgc)};
-
+  
   
 }
